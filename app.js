f6d9e776456433007fc2c0246de863db3ed797f5
Braunbart <hello@tillschander.com>
2014-03-25 21:28:27 +0000

    first files

---
+++ app.js
@@ -0,0 +1,57 @@
+var express = require('express'), 
+	cons = require('consolidate'),
+	server = express(),
+	FlickrAPI = require('flickrnode').FlickrAPI,
+	apiKey = require('./api-key');
+
+var api_key = apiKey.api_key;
+var api_secret = apiKey.api_secret;
+
+
+var flickr = new FlickrAPI(api_key, api_secret);
+
+server.engine('html', cons.mustache);
+server.set('view engine', 'html');
+server.set('views', __dirname + '/views');
+
+server.get("/login", function(req, res) {
+	//res.set('Content-Type', 'application/json');
+	flickr.getLoginUrl("write", null, function(error, url, frob) {
+		//var r = {frob: frob, url: url};
+		//res.write(JSON.stringify(r));
+		//res.end();
+		res.redirect(url);
+	});
+});
+
+server.get("/auth", function(req, res) {
+	res.set('Content-Type', 'application/json');
+	var frob = req.params.frob, r;
+	flickr.auth.getToken(frob, function(error, results) {
+		if (!error) {
+			r = results;
+		} else {
+			r = error;
+		}
+		res.write(JSON.stringify(r));
+		res.end();
+	});
+});
+
+server.get("/search/:term", function(req, res) {
+	flickr.photos.search({tags:req.params.term},  function(error, results) {
+    		if (!error) {
+			res.render('search', results);
+        	} else {
+                	r = error;
+	        	res.end(JSON.stringify(r));
+        	}
+	});
+});
+
+server.get("/", function(req, res) {
+	res.redirect('/search/kittens');
+});
+
+server.listen(10766);
+
